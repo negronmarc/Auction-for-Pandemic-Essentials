81217793c9f4cd195e98b3eaf7ed45256eac371f
KatieG <katieshake27@gmail.com>
2021-12-15 14:35:22 -0500

    refactoring

--- public/js/logout.js
+++ public/js/logout.js
@@ -13,19 +13,19 @@ const logout = async () => {
 
 document.querySelector('#logout').addEventListener('click', logout);
 
-async function logoutButtonHandler() {
-    const response = await fetch("", {
-      method: "POST",
-      headers: { "Content-Type": "" },
-    });
+// async function logoutButtonHandler() {
+//     const response = await fetch("", {
+//       method: "POST",
+//       headers: { "Content-Type": "" },
+//     });
   
-    if (response.ok) {
-      document.location.replace("/login");
-    } else {
-      alert(response.statusText);
-    }
-  }
+//     if (response.ok) {
+//       document.location.replace("/login");
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
   
-  document
-    .querySelector("#logout")
-    .addEventListener("click", logoutButtonHandler);
+//   document
+//     .querySelector("#logout")
+//     .addEventListener("click", logoutButtonHandler);
