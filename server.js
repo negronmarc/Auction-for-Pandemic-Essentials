0fc20bdd19b19272d0fb5cd3ca967d569c644c0e
KatieG <katieshake27@gmail.com>
2021-12-17 15:20:14 -0500

    restructuring handlebars

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-<<<<<<< HEAD
 // const helpers = require("./utils/helpers");
-=======
-//const helpers = require("./utils/helpers");
->>>>>>> e1a842cb9f8bbb3afa1b19583b410a74af448858
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
