0161485d50e9d27cb43da646a89a6c509a658d64
Marc Negron <negronmarc@gmail.com>
2021-12-17 10:46:47 -0500

    comments

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,20 @@
 const sequelize = require('../config/connection');
-const { Product } = require('../models');
+const { Categories, Products, User } = require('../models');
 
 const productData = require('./productData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  // Create a database or table for users   
+  // Assign this return data to a const var users
+
+  // Create a database table for category
+  // Assign this return data to a const var categories
+
+  // Loop through product data for each products 
+  // For each product pick random user primary key and random category primary key
+  // Assign it to foreign key user_id and category_id respectively 
 
   for (const product of productData) {
     await Product.create({
