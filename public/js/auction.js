65db4b836c62092d2d6bb8e88801f32d95b5d234
Marc Negron <negronmarc@gmail.com>
2021-12-21 14:52:12 -0500

    added function for auction.js

--- public/js/auction.js
+++ public/js/auction.js
@@ -1,8 +1,25 @@
+const sumBtn = document.getElementById('submit');
+const productId = parseInt(document.querySelector(".product-id").getAttribute("id"));
+const bidCount = parseInt(document.querySelector(".bid-count").getAttribute("data-bid-count"));
+const currentBid = parseInt(document.querySelector(".current-bid").getAttribute("data-current-bid"));
+console.log(typeof productId);
+console.log(typeof currentBid);
+console.log(typeof bidCount);
 
-const sumBtn = document.getElementById('submit-button')
+sumBtn.addEventListener("click", bidAmount);
 
-sumBtn.addEventListener("click", (event) => {
-    const textBox = document.getElementById('submit-box')
-    const bid = textBox.value
-    fetch 
-});
+async function bidAmount (event) {
+    event.preventDefault();
+    const response = await fetch(`/auction/${productId}`, {
+        method: "PUT", 
+        body: JSON.stringify({
+            bid_count: bidCount + 1,
+            current_bid: currentBid + 1,
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+    const updatedBid = response.json()
+    console.log(updatedBid);
+};
